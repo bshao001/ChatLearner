e81e25f1d97ef4f141e392bda736aaa6a37aadf5
bshao001 <bshao001@gmail.com>
2017-06-13 15:29:29 -0400

    Optimize the UI and allow the user to exit the program smoothly.

--- chatbot/botui.py
+++ chatbot/botui.py
@@ -7,20 +7,27 @@ from settings import PROJECT_ROOT
 from chatbot.tokenizeddata import TokenizedData
 from chatbot.botpredictor import BotPredictor
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def bot_ui():
-    data_file = os.path.join(PROJECT_ROOT, 'Data', 'Corpus', 'basic_conv.txt')
-    td = TokenizedData(seq_length=10, data_file=data_file)
+    dict_file = os.path.join(PROJECT_ROOT, 'Data', 'Result', 'dicts.pickle')
+    td = TokenizedData(seq_length=10, dict_file=dict_file)
 
     res_dir = os.path.join(PROJECT_ROOT, 'Data', 'Result')
     with tf.Session() as sess:
         predictor = BotPredictor(sess, td, res_dir, 'basic')
 
+        print("Welcome to Chat with ChatLearner!")
+        print("Type exit and press enter to end the conversation.")
         # Waiting from standard input.
         sys.stdout.write("> ")
         sys.stdout.flush()
         sentence = sys.stdin.readline()
         while sentence:
+            if sentence.strip() == 'exit':
+                print("Thank you for using ChatLearner. Bye.")
+                break
+
             dec_outputs = predictor.predict(sentence)
 
             word_ids = []
